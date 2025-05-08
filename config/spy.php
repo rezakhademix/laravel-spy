992274f5fa2d9bdb8a595e2f013d24b0c831bf9f
Reza Khademi <rezakhademix@gmail.com>
2025-05-08 09:01:50 +0330

    feat: fix php8.4 deprecation warning on passing null to explode() function parameter #2 which expected string

--- config/spy.php
+++ config/spy.php
@@ -1,10 +1,11 @@
 <?php
 
 return [
+    'table_name' => 'http_logs',
+    
     'enabled' => env('SPY_ENABLED', true),
     'db_connection' => env('SPY_DB_CONNECTION'),
-    'table_name' => 'http_logs',
 
-    'exclude_urls' => explode(',', env('SPY_EXCLUDE_URLS', null)),
+    'exclude_urls' => explode(',', env('SPY_EXCLUDE_URLS', '')),
     'obfuscates' => explode(',', env('SPY_OBFUSCATES', 'password')),
 ];
